27707970d774daf86f712ed1993bba7206bf79e4
Yasir349913 <yasirmaqsood534@gmail.com>
2025-07-31 12:25:00 -0700

    vite.config fix

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),
-    react()],
+  base: "./", // ✅ this is the fix
+  plugins: [tailwindcss(), react()],
 })
